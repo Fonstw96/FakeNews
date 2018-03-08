6dcf05bddd9e8957abbca063595bf06734f52f61
Unknown <fons_met_het_lange_e-mailadres@abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk.com>
2018-03-08 15:04:11 +0100

    I made money!
    https://www.youtube.com/watch?v=qMxX-QOV9tI

---
+++ Assets/Dev/Script/PlayerMoney.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoney : MonoBehaviour
+{
+    public int beginMoney = 100;
+    private int iMoney;
+    public Transform tPoster;
+    private const int iPosterCost = 10;
+
+    void Start ()
+    {
+        iMoney = beginMoney;
+    }
+
+    void Update ()
+    {
+        // if interaction button and enough money for a poster
+		if (Input.GetButtonDown("Fire1") && iMoney > iPosterCost)
+        {
+            // Poster money
+            iMoney -= iPosterCost;
+
+            Instantiate(tPoster, transform.position, new Quaternion());
+        }
+	}
+}
