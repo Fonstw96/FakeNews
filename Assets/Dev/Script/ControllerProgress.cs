6110738978aff98de4b17b9754e8f3512c818157
Unknown <fons_met_het_lange_e-mailadres@abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk.com>
2018-03-11 21:31:42 +0100

    UI pr 1/3: Progressbar

---
+++ Assets/Dev/Script/ControllerProgress.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerProgress : MonoBehaviour
+{
+    public int iRedVillagers = 0;
+    public int iBlueVillagers = 0;
+
+    public Transform tRedUIBar;
+    private RectTransform tBarSize;
+
+    void Start()
+    {
+        // Take red bar's size component, for easier coding later on
+        if (tRedUIBar != null)
+            tBarSize = tRedUIBar.GetComponent<RectTransform>();
+    }
+
+    private void ChangeProgress()
+    {
+        float fRedvsBlue;
+
+        // Just in case we end up dividing by zero...
+        if (iRedVillagers == 0 && iBlueVillagers == 0)
+            fRedvsBlue = .5f;
+        // There won't be any scary divisions? Good, calculate % red villagers vs % blue villagers and store that!
+        else
+            fRedvsBlue = iRedVillagers / (float)(iRedVillagers + iBlueVillagers);
+
+        // Scale red bar's width according to % red villagers versus % blue villagers
+        tBarSize.sizeDelta = new Vector2(600 * fRedvsBlue, 50);
+    }
+}
