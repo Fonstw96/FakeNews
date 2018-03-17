e5ce33c65052429438aeb2087da4fa3e543a87c4
Unknown <fons_met_het_lange_e-mailadres@abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk.com>
2018-03-17 18:21:25 +0100

    I made some time for you
    + Time neatly displayed on UI
    + Level resets after time runs out

---
+++ Assets/Dev/Script/TimeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TimeController : MonoBehaviour
+{
+    public float fStartTime = 500;
+    private Text tText;
+
+    private void Start()
+    {
+        tText = GetComponent<Text>();
+    }
+
+    void Update ()
+    {
+        // Take away 1 frame of milliseconds
+        fStartTime -= Time.deltaTime;
+
+        // Out of time? Reset the level
+        if (fStartTime <= 0)
+            SceneManager.LoadScene("Main");
+
+        // Change time text so the player knows
+        tText.text = Mathf.Ceil(fStartTime).ToString();
+	}
+}
