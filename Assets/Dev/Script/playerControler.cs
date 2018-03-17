aadda3a4a3be94ca1233255f86d5c102b1cf2399
Unknown <mickteun@gmail.com>
2018-03-17 19:52:28 +0100

    Player1 Animation

--- Assets/Dev/Script/playerControler.cs
+++ Assets/Dev/Script/playerControler.cs
@@ -7,12 +7,18 @@ public class playerControler : MonoBehaviour
     public string sPlayerNo = "1";
     public float moveSpeed;
     public Rigidbody theRB;
+
+    public GameObject character;
+    private Animator anim;
     //public float jumpForce;
 
     // Use this for initialization
     void Start()
     {
         theRB = GetComponent<Rigidbody>();
+        anim = character.GetComponent<Animator>();
+
+        anim.Play("Idle");
     }
 
     // Update is called once per frame
@@ -24,5 +30,18 @@ public class playerControler : MonoBehaviour
         //{
         //    theRB.velocity = new Vector3(theRB.velocity.x, jumpForce, theRB.velocity.z);
         //}
+
+        if (Input.GetAxis("Horizontal" + sPlayerNo) > 0)
+        {
+            Debug.Log("right");
+            anim.Play("WalkRight");
+        }
+
+        if (Input.GetAxis("Horizontal" + sPlayerNo) < 0)
+        {
+            Debug.Log("left");
+            anim.Play("WalkLeft");
+        }
+
     }
 }
