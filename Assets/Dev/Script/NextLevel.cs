eaf727922157b12f42fb52fc3c2afa3e83ca4657
Unknown <mickteun@gmail.com>
2018-03-18 02:15:04 +0100

    Intro scene

---
+++ Assets/Dev/Script/NextLevel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NextLevel : MonoBehaviour {
+
+    void Update()
+    {
+        if (Input.GetButton("Interact1")
+        {
+            
+        }
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    //void Start()
+    //{
+    //    Button b = gameObject.GetComponent<Button>();
+    //    b.onClick.AddListener(delegate () { this.LoadLevel(); });
+    //    b.onClick.AddListener(delegate () { print("Button is geklikked"); });
+    //}
+
+    
+
+
+}
